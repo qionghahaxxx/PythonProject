4ef5083d95dd3557f2f652aaed4ac8071bb31200
qionghahaxxx <2361722734@qq.com>
2025-03-17 16:01:15 +0800

    脚本优化

--- ty_api_test/TestCase/test_tzb.py
+++ ty_api_test/TestCase/test_tzb.py
@@ -48,28 +48,28 @@ class TestTzb:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_lx6(self):
         """立项待办任务-二级单位稽核人稽核通过"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.lx_jude_pass(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_lx7(self):
         """立项待办任务-二级单位稽核人稽核驳回"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.lx_jude_reject(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_lx8(self):
         """立项待办任务-总部审定人稽核通过"""
-        db = Dbjh('User2', 'Password2')
+        db = DbJh('User2', 'Password2')
         try:
             db.lx_jude_pass(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_lx9(self):
         """立项待办任务-总部审定人稽核驳回"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.lx_jude_reject(1)
         except Exception as e:
@@ -157,28 +157,28 @@ class TestTzb:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_ky8(self):
         """可研待办任务-二级单位稽核人稽核通过"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.ky_jude_pass(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_ky9(self):
         """可研待办任务-二级单位稽核人稽核驳回"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.ky_jude_reject(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_ky10(self):
         """可研待办任务-总部审定人稽核通过"""
-        db = Dbjh('User2', 'Password2')
+        db = DbJh('User2', 'Password2')
         try:
             db.ky_jude_pass(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
     def test_ky11(self):
         """可研待办任务-总部审定人稽核驳回"""
-        db = Dbjh('User3', 'Password3')
+        db = DbJh('User3', 'Password3')
         try:
             db.ky_jude_reject(1)
         except Exception as e:
@@ -220,6 +220,174 @@ class TestTzb:
         """验证添加投资预算实施进度，提交稽核成功"""
         try:
             Ss.ss_project_investment('测试')
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss7(self):
+        """验证实施许可令待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.ss_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss8(self):
+        """验证实施许可令待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.ss_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss9(self):
+        """验证实施许可令待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.ss_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss10(self):
+        """验证实施许可令待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.ss_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss11(self):
+        """验证项目公司待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.company_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss12(self):
+        """验证项目公司待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.company_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss13(self):
+        """验证项目公司待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.company_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss14(self):
+        """验证项目公司待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.company_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss15(self):
+        """验证项目招投标及合同文件待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.contract_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss16(self):
+        """验证项目招投标及合同文件待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.contract_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss17(self):
+        """验证项目招投标及合同文件待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.contract_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss18(self):
+        """验证项目招投标及合同文件待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.contract_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss19(self):
+        """验证建设实施进度待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.progress_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss20(self):
+        """验证建设实施进度待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.progress_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss21(self):
+        """验证建设实施进度待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.progress_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss22(self):
+        """验证建设实施进度待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.progress_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss23(self):
+        """验证合规性手续待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.compliance_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss24(self):
+        """验证合规性手续待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.compliance_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss25(self):
+        """验证合规性手续待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.compliance_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss26(self):
+        """验证合规性手续待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.compliance_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss27(self):
+        """验证预算实施进度待办任务，二级单位稽核人稽核通过"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.asset_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss28(self):
+        """验证预算实施进度待办任务，二级单位稽核人稽核驳回"""
+        db = DbJh('User3', 'Password3')
+        try:
+            db.asset_jude_reject(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss29(self):
+        """验证预算实施进度待办任务，总部审定人稽核通过"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.asset_jude_pass(1)
+        except Exception as e:
+            log.error(f"测试过程中出现异常，异常信息为：{e}")
+    def test_ss30(self):
+        """验证预算实施进度待办任务，总部审定人稽核驳回"""
+        db = DbJh('User2', 'Password2')
+        try:
+            db.asset_jude_reject(1)
         except Exception as e:
             log.error(f"测试过程中出现异常，异常信息为：{e}")
 
--- ty_api_test/page/daibanjihe.py
+++ ty_api_test/page/daibanjihe.py
@@ -1,5 +1,4 @@
 #待办稽核相关操作
-from time import sleep
 
 import requests
 from ty_api_test.common.logger import *
@@ -9,7 +8,7 @@ from ty_api_test.common.readapi import *
 import json
 
 
-class Dbjh:
+class DbJh:
     """待办任务相关操作封装"""
     def __init__(self,user,password):
         self.authorization,self.userid = login(u=user, p=password)
@@ -177,7 +176,7 @@ class Dbjh:
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
 
-    def ssxkl_jude_pass(self,excuted_num=1):
+    def ss_jude_pass(self,excuted_num=1):
         """实施许可令待办任务稽核通过"""
         authorization = self.authorization
         host = Readconfig('HOST-TZB').host
@@ -201,6 +200,33 @@ class Dbjh:
                 assert response.json()['code'] == 200
                 #print(response.json())
                 log.debug(f'第{i+1}条实施许可令待办任务审核通过成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
+    def ss_jude_reject(self,excuted_num=1):
+        """实施许可令待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['实施许可令稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("3")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] ==200
+                print(response.json())
+                log.debug(f'第{i+1}条实施许可令待办任务审核驳回成功')
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
@@ -231,6 +257,33 @@ class Dbjh:
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
+    def company_jude_reject(self,excuted_num=1):
+        """项目公司待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['项目公司稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("4")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] == 200
+                print(response.json())
+                log.debug(f'第{i+1}条项目公司待办任务审核驳回成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
     def contract_jude_pass(self,excuted_num=1):
         """招投标及合同文件待办任务稽核通过"""
         authorization = self.authorization
@@ -255,6 +308,33 @@ class Dbjh:
                 assert response.json()['code'] == 200
                 #print(response.json())
                 log.debug(f'第{i+1}条招投标及合同文件待办任务审核通过成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
+    def contract_jude_reject(self,excuted_num=1):
+        """招投标及合同文件待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['招投标及合同文件稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("5")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] ==200
+                print(response.json())
+                log.debug(f'第{i+1}条招投标及合同文件待办任务审核驳回成功')
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
@@ -285,6 +365,33 @@ class Dbjh:
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
+    def progress_jude_reject(self,excuted_num=1):
+        """建设实施进度待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['建设实施进度稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("6")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] == 200
+                print(response.json())
+                log.debug(f'第{i+1}条建设实施进度待办任务审核驳回成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
     def compliance_jude_pass(self,excuted_num=1):
         """合规性手续待办任务稽核通过"""
         authorization = self.authorization
@@ -309,6 +416,33 @@ class Dbjh:
                 assert response.json()['code'] == 200
                 #print(response.json())
                 log.debug(f'第{i+1}条合规性手续待办任务审核通过成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
+    def compliance_jude_reject(self,excuted_num=1):
+        """合规性手续待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['合规性手续稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("7")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] ==200
+                print(response.json())
+                log.debug(f'第{i+1}条合规性手续待办任务审核驳回成功')
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
@@ -339,84 +473,111 @@ class Dbjh:
                 i+=1
         else:
             log.debug(f"抱歉，目前只有{num}条待办任务")
+    def asset_jude_reject(self,excuted_num=1):
+        """预算实施进度待办任务稽核驳回"""
+        authorization = self.authorization
+        host = Readconfig('HOST-TZB').host
+        api = Api('api')['预算实施进度稽核']
+        url = f"https://{host}{api}"
+        headers = {
+            "Authorization": f"Bearer {authorization}",
+            'Content-Type': 'application/json'
+        }
+        businessId_list, taskId_list, num = self.get_task("8")
+        if num>=excuted_num:
+            for i in range(excuted_num):
+                data = json.dumps({
+                    "status": "REJECTED",
+                    "result": "拒绝",
+                    "businessId": f"{businessId_list[i]}",
+                    "taskId": f"{taskId_list[i]}",
+                    "comments": "稽核测试驳回"
+                })
+                response = requests.post(url, headers=headers, data=data)
+                assert response.json()['code'] ==200
+                print(response.json())
+                log.debug(f'第{i+1}条预算实施进度待办任务审核驳回成功')
+                i+=1
+        else:
+            log.debug(f"抱歉，目前只有{num}条待办任务")
 
-# db1 = Dbjh('User3','Password3')
-#db2 = Dbjh('User2','Password2')
+# db1 = DbJh('User3','Password3')
+#db2 = DbJh('User2','Password2')
 #User3 = wangyong6     #二级单位稽核人
 #User2 = kangle1        #总部审定人
 #User1 = caomeng
 if __name__ == '__main__':
 
     # #1.1二级单位稽核人立项稽核
-    # db1 = Dbjh('User3', 'Password3')
+    # db1 = DbJh('User3', 'Password3')
     # db1.lx_jude_pass(4)
     # db1.get_task("1")
     # sleep(1)
     # #1.2总部审定人立项审定
-    # db2 = Dbjh('User2','Password2')
+    # db2 = DbJh('User2','Password2')
     # db2.lx_jude_pass(4)
     # db2.get_task("1")
 
     #
     # # 2.1可研稽核:
-    # db1 = Dbjh('User3', 'Password3')
+    # db1 = DbJh('User3', 'Password3')
     # db1.ky_jude_pass(4)
     # db1.get_task("2")
     # # 2.2可研审定:
-    # db2 = Dbjh('User2','Password2')
+    # db2 = DbJh('User2','Password2')
     # db2.ky_jude_pass(2)
     # db2.get_task("2")
 
     # # 3.1实施许可令稽核:
-    # db1 = Dbjh('User3', 'Password3')
-    # db1.ssxkl_jude_pass(1)
+    db1 = DbJh('User3', 'Password3')
+    db1.ss_jude_pass(1)
     # db1.get_task("3")
     # # 3.2实施许可令审定:
-    # db2 = Dbjh('User2', 'Password2')
-    # db2.ssxkl_jude_pass(1)
+    # db2 = DbJh('User2', 'Password2')
+    # db2.ss_jude_pass(1)
     # db2.get_task("3")
 
     # 4.1项目公司稽核:
-    # db1 = Dbjh('User3', 'Password3')
+    # db1 = DbJh('User3', 'Password3')
     # db1.company_jude_pass(1)
     # db1.get_task("4")
     # # 4.2项目公司审定:
-    # db2 = Dbjh('User2', 'Password2')
+    # db2 = DbJh('User2', 'Password2')
     # db2.company_jude_pass(1)
     # db2.get_task("4")
 
     # 5.1招投标及合同文件稽核:
-    # db1 = Dbjh('User3', 'Password3')
+    # db1 = DbJh('User3', 'Password3')
     # db1.contract_jude_pass(1)
     # db1.get_task("5")
     # # 5.2招投标及合同文件审定:
-    # db2 = Dbjh('User2', 'Password2')
+    # db2 = DbJh('User2', 'Password2')
     # db2.contract_jude_pass(1)
     # db2.get_task("5")
 
     # 6.1建设实施进度稽核:
-    # db1 = Dbjh('User3', 'Password3')
+    # db1 = DbJh('User3', 'Password3')
     # db1.progress_jude_pass(1)
     # db1.get_task("6")
     # # 6.2建设实施进度审定:
-    # db2 = Dbjh('User2', 'Password2')
+    # db2 = DbJh('User2', 'Password2')
     # db2.progress_jude_pass(1)
     # db2.get_task("6")
 
-    # 7.1合规性手续稽核稽核:
-    # db1 = Dbjh('User3', 'Password3')
+    # 7.1合规性手续稽核:
+    # db1 = DbJh('User3', 'Password3')
     # db1.compliance_jude_pass(1)
     # db1.get_task("7")
     # # 7.2合规性手续稽核审定:
-    # db2 = Dbjh('User2', 'Password2')
+    # db2 = DbJh('User2', 'Password2')
     # db2.compliance_jude_pass(1)
     # db2.get_task("7")
 
-    # 8.1预算实施进度稽核稽核:
-    db1 = Dbjh('User3', 'Password3')
+    # 8.1预算实施进度稽核:
+    db1 = DbJh('User3', 'Password3')
     db1.asset_jude_pass(1)
     db1.get_task("8")
     # # 8.2预算实施进度审定:
-    # db2 = Dbjh('User2', 'Password2')
+    # db2 = DbJh('User2', 'Password2')
     # db2.asset_jude_pass(1)
     # db2.get_task("8")
